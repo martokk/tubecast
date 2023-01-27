60b8f6a34d35bfaf6d6228a2497708070ad47f7e
martokk <25113632+martokk@users.noreply.github.com>
2023-01-26 18:06:27 -0600

    removing uneeded tests

--- tests/api/v1/endpoints/test_feed_endpoints.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-
-
-def test_get_rss_not_found(client: TestClient) -> None:
-    response = client.get("/feed/non_existent_source")
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Not Found"
