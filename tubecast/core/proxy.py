9466470d6aa612576d067152e1ae2e2ff06323d3
martokk <25113632+martokk@users.noreply.github.com>
2023-02-04 11:50:34 -0600

    add more logging to proxy

--- tubecast/core/proxy.py
+++ tubecast/core/proxy.py
@@ -1,7 +1,10 @@
 import httpx
+from fastapi import status, HTTPException
 from fastapi.requests import Request
 from fastapi.responses import StreamingResponse
 from starlette.background import BackgroundTask
+
+from tubecast import logger
 
 client = httpx.AsyncClient()
 
@@ -29,6 +32,11 @@ async def reverse_proxy(url: str, request: Request) -> StreamingResponse:
         rp_response = await client.send(rp_request, stream=True)
     except httpx.ConnectError as e:
         raise ValueError("Invalid URL") from e
+
+    if rp_response.status_code != status.HTTP_200_OK:
+        logger.error(f"Reverse proxy request failed with status code {rp_response.status_code}")
+        logger.error(f"{rp_response=}")
+        raise HTTPException(status_code=rp_response.status_code)
 
     return StreamingResponse(
         rp_response.aiter_raw(),
