91b30fbd2c8adaac3d7fdc8f60c8fbf9e6a1a36d
martokk <25113632+martokk@users.noreply.github.com>
2023-02-07 17:34:42 -0600

    added temp_log foe error handling of live rumble videos

--- tubecast/handlers/rumble.py
+++ tubecast/handlers/rumble.py
@@ -10,6 +10,7 @@ from tubecast.handlers.extractors.rumble import (
     CustomRumbleIE,
 )
 from tubecast.paths import LOG_FILE as _LOG_FILE
+from tubecast.paths import TEMP_LOG_FILE as _TEMP_LOG_FILE
 from tubecast.services.ytdlp import YDL_OPTS_BASE
 
 from .base import ServiceHandler
@@ -18,7 +19,8 @@ from .base import ServiceHandler
 logger = _logger.bind(name="logger")
 logger.add(_LOG_FILE, level="WARNING", rotation="10 MB")
 
-# import re
+temp_logger = _logger.bind(name="logger")  # TODO: This is temporary. Remove this.
+temp_logger.add(_TEMP_LOG_FILE, level="CRITICAL", rotation="10 MB")
 
 
 class RumbleHandler(ServiceHandler):
@@ -180,6 +182,13 @@ class RumbleHandler(ServiceHandler):
 
         # TODO: Finalize this solution after some time/testings to see why this happens
         # Handle when media_filesize in below 2MB
+        if "live" in entry_info_dict["title"].lower():  # TODO: This is temporary. Remove
+            logger.critical(
+                f"'Live' found in title. The first appearance is the one causing issues."
+            )
+            logger.critical(
+                f"\n{entry_info_dict['title']=}\n{media_filesize=}\n{media_url=}\n\n{entry_info_dict=}\n\n-----------------\n\n"
+            )
         if (
             media_filesize > 0  # 0 bytes
             and media_filesize < 2 * 1024 * 1024  # 2MB
